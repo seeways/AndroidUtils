42e91c6ab251b5dd658a7bdf71e0e894f618670e
seeways <1876665310@qq.com>
2019-10-31 09:24:23 +0800

    增加time种类

--- myutils/src/main/java/com/jty/myutils/utils/TimeUtils.java
+++ myutils/src/main/java/com/jty/myutils/utils/TimeUtils.java
@@ -14,10 +14,35 @@ public class TimeUtils {
     public static String getTimeStemp() {
         return new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
     }
-    //获得当前时间
-    public static String getTime(){
 
+    // 获取当日时间戳
+    public static String getDayTimeStemp() {
+        return new SimpleDateFormat("yyyyMMdd").format(new Date());
+    }
+
+    // 获取当月时间戳
+    public static String getMonthStemp() {
+        return new SimpleDateFormat("yyyyMM").format(new Date());
+    }
+
+    //获得当前时间
+    public static String getTime() {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    }
+
+    //获得当前日期
+    public static String getDay() {
+        return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    }
+
+    //获得当月時間
+    public static String getMonth() {
+        return new SimpleDateFormat("yyyy-MM").format(new Date());
+    }
+
+    //获得当年时间
+    public static String getYear() {
+        return new SimpleDateFormat("yyyy").format(new Date());
     }
 
 
@@ -31,6 +56,7 @@ public class TimeUtils {
     public static String getDayTime() {
         return year + "/" + month + "/" + days;
     }
+
     public static String getLikeMonth() {
         return year + "/" + month + "%";
     }
