af550529dc3de009db142a202eee1960f1884d4c
Taoyuan <1876665310@qq.com>
2023-04-25 11:47:01 +0800

    unicode转string

--- myutils/src/main/java/com/jty/myutils/utils/StringUtils.java
+++ myutils/src/main/java/com/jty/myutils/utils/StringUtils.java
@@ -1,5 +1,8 @@
 package com.jty.myutils.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author TaoYuan
  * @time 2017/3/17 0017
@@ -9,6 +12,7 @@ package com.jty.myutils.utils;
 public class StringUtils {
     /**
      * 判空(空格也算空)
+     *
      * @param s s1
      * @return r
      */
@@ -18,14 +22,32 @@ public class StringUtils {
 
     /**
      * 字符串是否相等
-     * @param a s1
-     * @param b s2
+     *
+     * @param a            s1
+     * @param b            s2
      * @param isIgnoreCase 是否忽略大小写
      * @return r
      */
-    public static boolean isEquals(String a, String b,boolean isIgnoreCase) {
+    public static boolean isEquals(String a, String b, boolean isIgnoreCase) {
         return (a == b) || (b != null) && (a.length() == b.length()) && a.regionMatches(isIgnoreCase, 0, b, 0, b.length());
     }
 
+    /**
+     * unicode转string
+     *
+     * @param unicode unicode
+     * @return string
+     */
+    public static String unicodeToString(String unicode) {
+
+        Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
+        Matcher matcher = pattern.matcher(unicode);
+        char ch;
+        while (matcher.find()) {
+            ch = (char) Integer.parseInt(matcher.group(2), 16);
+            unicode = unicode.replace(matcher.group(1), ch + "");
+        }
+        return unicode;
+    }
 
 }
